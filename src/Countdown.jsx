0631e94bf7b08b22d19f63743add85f7b4e0f9b4
Carolina Acuna <carolina.acuna90@gmail.com>
2023-06-25 19:21:14 +0200

    needs style and better input date

---
+++ src/Countdown.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import "./Countdown.css";
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
+
+export const Countdown = () => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [inputDate, setInputeDate] = useState("22 July 2023");
+  const [currentDate, setCurrentDate] = useState(inputDate);
+
+  useEffect(() => {
+    const changingDate = new Date(inputDate);
+    const currentDate = new Date();
+    const totalSeconds = (changingDate - currentDate) / 1000;
+
+    setDays(formatTime(Math.floor(totalSeconds / 3600 / 24)));
+    setHours(Math.floor(totalSeconds / 3600) % 24);
+    setMinutes(Math.floor(totalSeconds / 60) % 60);
+    setSeconds(Math.floor(totalSeconds % 60));
+  }, [currentDate]);
+
+  function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+  }
+
+  const onChangeHandler = (event) => {
+    setInputeDate(event.target.value);
+  };
+
+  const onClickHandler = () => {
+    setCurrentDate(inputDate);
+  };
+
+  return (
+    <div className='countdown-container'>
+      <div className='countdown-values'>
+        <div className='cowntdown-value'>
+          <p className='big-text'>{days}</p>
+          <span>days</span>
+        </div>
+        <div className='cowntdown-value'>
+          <p className='big-text'>{hours}</p>
+          <span>hours</span>
+        </div>
+        <div className='cowntdown-value'>
+          <p className='big-text'>{minutes}</p>
+          <span>minutes</span>
+        </div>
+        <div className='cowntdown-value'>
+          <p className='big-text'>{seconds}</p>
+          <span>seconds</span>
+        </div>
+      </div>
+      <div className='countdown-input-button'>
+        <input className='countdown-input' onChange={onChangeHandler} />
+        <button className='cowntdown-button' onClick={onClickHandler}>
+          Countdown
+        </button>
+      </div>
+    </div>
+  );
+};
